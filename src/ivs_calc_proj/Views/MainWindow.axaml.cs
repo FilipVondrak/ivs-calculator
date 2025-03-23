38eacee8d01656210298246aee1a28d22c6180bb
Filip Vondrák <90453814+FilipVondrak@users.noreply.github.com>
2025-03-23 10:07:56 +0100

    bug fixes - dependancies and title bar

--- src/ivs_calc_proj/Views/MainWindow.axaml.cs
+++ src/ivs_calc_proj/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Platform;
 
 namespace ivs_calc_proj.Views;
 
@@ -9,6 +12,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        if (!OperatingSystem.IsWindows())
+        {
+            TitleBarTitle.IsVisible = false;
+            TitleBarButtons.IsVisible = false;
+            DisplayBorder.Padding = new Thickness(0);
+        }
     }
 
     private void CloseApp(object? sender, RoutedEventArgs e) => Close();
@@ -16,5 +26,6 @@ public partial class MainWindow : Window
     private void MinimizeApp(object? sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
 
     private void MaximizeApp(object? sender, RoutedEventArgs e) => WindowState = WindowState.Maximized;
-    
+
+    private void TitleBarHold(object? sender, PointerPressedEventArgs e) => BeginMoveDrag(e);
 }
