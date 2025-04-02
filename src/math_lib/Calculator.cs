bb5d47213c7039268251196ee69be52041e08e88
Oleksandr <gamealex2510@gmail.com>
2025-04-02 22:41:31 +0200

    implementace zakladnich testu aby Krouz mohl zacit programovat zakladni funkcnost. Vytcoril jsem tridu pro matematickou knihovnu. Krouzi, tam muzes a sorry za zpozdeni.

---
+++ src/math_lib/Calculator.cs
@@ -0,0 +1,59 @@
+﻿namespace math_lib;
+
+public class Calculator : ICalculator
+{
+    public double Add(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Subtract(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Multiply(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Divide(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Factorial(int n)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Power(double baseNum, int exponent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Root(double baseNum, int rootDegree)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Log(double baseNum, double logBase)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Sin(double angle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Cos(double angle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Tan(double angle)
+    {
+        throw new NotImplementedException();
+    }
+}
