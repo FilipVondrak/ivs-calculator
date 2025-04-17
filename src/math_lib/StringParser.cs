9e8937fc1d23587db7a04659ffa71661e526f436
Mykola Bondar <bolakondar@gmail.com>
2025-04-17 15:19:45 +0200

    inicialisation StringParser

--- src/math_lib/StringParser.cs
+++ src/math_lib/StringParser.cs
@@ -1,6 +1,14 @@
 ﻿namespace math_lib;
 
-public class StringParser
+public class StringParser : IStringParser
 {
-    
+    public string CalculateBracket(string expression)
+    {
+        return "";
+    }
+
+    public string SolveExpression(string expression)
+    {
+        return "";
+    }
 }
