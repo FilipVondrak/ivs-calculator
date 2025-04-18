d4c107bc82a0823652da2d39ec055c94ea06cf6a
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-04-18 23:31:51 +0200

    FIX: fixed behaviour of delete button and operations

--- src/ivs_calc_proj/ViewModels/MainWindowViewModel.cs
+++ src/ivs_calc_proj/ViewModels/MainWindowViewModel.cs
@@ -53,10 +53,34 @@ public partial class MainWindowViewModel : ViewModelBase
     }
 
     [RelayCommand]
+    private void AddRootOfOperation(string newOperation)
+    {
+        if (Expression.Length>0 && !(char.IsNumber(Expression[^1]) || Expression[^1]==' '))
+        {
+            return;
+        }
+
+        if ((Expression.Length > 0 && !char.IsNumber(Expression[^1]))||Expression.Length==0)
+        {
+            AddNumber("2");
+        }
+
+        Expression += $"{newOperation}";
+        ExpressionChanged();
+    }
+
+    [RelayCommand]
     private void AddUnaryOperation(string newOperation)
     {
-        if (Expression.Length>0 && newOperation == "√" && Expression[^1] != ' ')
+        // correctly formats the string
+        // example: expression is "55" and newOperation is "sin", then ensures there is multiplication between these
+        if (Expression.Length>0 && Expression[^1] != ' ' && newOperation!="^")
             AddBinaryOperation("*");
+
+        // ensures that there is a number before adding the ^ operation
+        if (newOperation=="^" && (Expression.Length>0 && !char.IsNumber(Expression[^1]) || Expression.Length==0))
+            return;
+
         Expression += $"{newOperation}";
         ExpressionChanged();
     }
@@ -77,9 +101,21 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             Expression=Expression.Substring(0, Expression.Length - 3);
         }
-        else if (Expression[^1] == '(' && Expression[^2] == ' ')
+        else if (Expression[^1] == '(')
         {
-            Expression=Expression.Substring(0, Expression.Length - 4);
+            if(Expression.Length==1)
+                Expression=Expression.Substring(0, Expression.Length - 1);
+            else if(Expression[^2] == ' ')
+                Expression=Expression.Substring(0, Expression.Length - 4);
+        }
+        else if (Expression[^1] == '√' && Expression[^2] == '2')
+        {
+            if (Expression.Length>2 && Expression[^3] == ' ')
+                Expression=Expression.Substring(0, Expression.Length - 2);
+            else if (Expression.Length==2)
+                Expression=Expression.Substring(0, Expression.Length - 2);
+            else
+                Expression=Expression.Substring(0, Expression.Length - 1);
         }
         else
         {
