fe852567a29c815b4d58b612d1a2889bff537d06
Oleksandr Bondar <gamealex2510@gmail.com>
2025-04-23 18:27:54 +0200

    Done: math_lib complete tests

--- src/math_tests/MathLibUnitTestTdd.cs
+++ src/math_tests/MathLibUnitTestTdd.cs
@@ -10,12 +10,12 @@ public class MathLibUnitTestTdd
     private readonly ICalculator _calculator = new Calculator();
 
     private readonly ITestOutputHelper _output;
-    
+
     public MathLibUnitTestTdd(ITestOutputHelper output)
     {
         _output = output;
     }
-    
+
     [Fact]
     public void Add_Basic1()
     {
@@ -49,6 +49,13 @@ public class MathLibUnitTestTdd
     {
         var result = _calculator.Add(0.0000m, 0);
         Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Add_decimals1()
+    {
+        var result = _calculator.Add(0.99999m, 0.11111m);
+        Assert.Equal(1.1111m, result);
     }
 
     [Fact]
@@ -87,6 +94,20 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Subtract_decimals1()
+    {
+        var result = _calculator.Subtract(0.0000m, 0.001m);
+        Assert.Equal(-0.001m, result);
+    }
+
+    [Fact]
+    public void Subtract_decimals2()
+    {
+        var result = _calculator.Subtract(-1.0177m, 0.0019m);
+        Assert.Equal(-1.0196m, result);
+    }
+
+    [Fact]
     public void Multiply_Basic1()
     {
         var result = _calculator.Multiply(2, 9);
@@ -122,6 +143,20 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Multiply_decimals1()
+    {
+        var result = _calculator.Multiply(0.99999m, 0.99999m);
+        Assert.Equal(0.99998m, result);
+    }
+
+    [Fact]
+    public void Multiply_decimals2()
+    {
+        var result = _calculator.Multiply(-1.1234m, 1.1234m);
+        Assert.Equal(-1.26203m, result);
+    }
+
+    [Fact]
     public void Divide_Basic1()
     {
         var result = _calculator.Divide(2, 9);
@@ -150,9 +185,37 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
-    public void Divide_Basic5()
+    public void Divide_zero()
     {
         Assert.Throws<DivideByZeroException>(() => _calculator.Divide((decimal)0.0000, 0));
+    }
+
+    [Fact]
+    public void Divide_decimals1()
+    {
+        var result = _calculator.Divide(1.53m, 3.1222m);
+        Assert.Equal(0.49004m, result);
+    }
+
+    [Fact]
+    public void Divide_decimals2()
+    {
+        var result = _calculator.Divide(11.78m, 7.811m);
+        Assert.Equal(1.50813m, result);
+    }
+
+    [Fact]
+    public void Divide_decimals3()
+    {
+        var result = _calculator.Divide(1.5m, 9m);
+        Assert.Equal(0.16667m, result);
+    }
+
+    [Fact]
+    public void Divide_decimals4()
+    {
+        var result = _calculator.Divide(9m, 1.89m);
+        Assert.Equal(4.7619m, result);
     }
 
     [Fact]
@@ -197,6 +260,41 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Power_Basic3()
+    {
+        var result = _calculator.Power(2, 0);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Power_decimals1()
+    {
+        var result = _calculator.Power(2, 2.9);
+        Assert.Equal(7.46426, result);
+    }
+
+    [Fact]
+    public void Power_decimals2()
+    {
+        var result = _calculator.Power(2, -2.9);
+        Assert.Equal(0.13397, result);
+    }
+
+    [Fact]
+    public void Power_decimals3()
+    {
+        var result = _calculator.Power(29, -1.234);
+        Assert.Equal(0.01568, result);
+    }
+
+    [Fact]
+    public void Power_decimals4()
+    {
+        var result = _calculator.Power(2.999, 2);
+        Assert.Equal(0, result.CompareTo(8.994));
+    }
+
+    [Fact]
     public void Root_Basic1()
     {
         var result = _calculator.Root(16, 2);
@@ -208,6 +306,27 @@ public class MathLibUnitTestTdd
     {
         var result = _calculator.Root(-8, 3);
         Assert.Equal(-2, result);
+    }
+
+    [Fact]
+    public void Root_decimals1()
+    {
+        var result = _calculator.Root(0.235m, 2);
+        Assert.Equal(0.48477m, result);
+    }
+
+    [Fact]
+    public void Root_decimals2()
+    {
+        var result = _calculator.Root(0.299m, 3);
+        Assert.Equal(0.66869m, result);
+    }
+
+    [Fact]
+    public void Root_decimals3()
+    {
+        var result = _calculator.Root(-29.029m, 29);
+        Assert.Equal(-1.12316m, result);
     }
 
     [Fact]
@@ -238,10 +357,31 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Log_decimals1()
+    {
+        var result = _calculator.Log(2.00333m, 2);
+        _output.WriteLine("Actual: " + result);
+        _output.WriteLine("Expected: " + 1.0024m);
+        Assert.Equal(0, result.CompareTo(1.0024m));
+    }
+
+    [Fact]
+    public void Log_decimals2()
+    {
+        var result = _calculator.Log(0.5m, 2);
+        _output.WriteLine("Actual: " + result);
+        _output.WriteLine("Expected: " + -1m);
+        Assert.Equal(0, result.CompareTo(-1m));
+    }
+
+    [Fact]
     public void Log_Exceptions()
     {
+        _output.WriteLine("value can not be 0");
         Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Log(0, 1));
+        _output.WriteLine("base can not be negative");
         Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Log(1, -1));
+        _output.WriteLine("value can not be negative");
         Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Log(-1, 1));
     }
 
@@ -274,6 +414,13 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Sin_420()
+    {
+        var result = _calculator.Sin(420);
+        Assert.Equal(0.86603m, result);
+    }
+
+    [Fact]
     public void Sin_90()
     {
         var result = _calculator.Sin(90);
@@ -298,6 +445,13 @@ public class MathLibUnitTestTdd
     public void Sin_360()
     {
         var result = _calculator.Sin(360);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Sin_720()
+    {
+        var result = _calculator.Sin(720);
         Assert.Equal(0, result);
     }
 
@@ -358,6 +512,13 @@ public class MathLibUnitTestTdd
     }
 
     [Fact]
+    public void Cos_720()
+    {
+        var result = _calculator.Cos(720);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
     public void Tan_0()
     {
         var result = _calculator.Tan(0);
@@ -410,5 +571,12 @@ public class MathLibUnitTestTdd
         var result = _calculator.Tan(360);
         Assert.Equal(0, result);
     }
-
+    
+    
+    [Fact]
+    public void Tan_720()
+    {
+        var result = _calculator.Tan(720);
+        Assert.Equal(0, result);
+    }
 }
