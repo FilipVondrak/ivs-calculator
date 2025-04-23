a7bc4d436461c376013c73046a6f4d450575e63e
Oleksandr Bondar <gamealex2510@gmail.com>
2025-04-23 20:58:34 +0200

    FIX: Changed interface and class decrease complexity by separated methods

--- src/math_lib/IStringParser.cs
+++ src/math_lib/IStringParser.cs
@@ -3,17 +3,104 @@
 public interface IStringParser
 {
     /// <summary>
-    /// Returns the cosine value of a specific angle in radians
+    /// Returns the new expression with solved expression in deepest brackets.
     /// </summary>
-    /// <param name="angle">Angle in radians</param>
-    /// <returns>The cosine value of <paramref name="angle"/></returns>
-    string CalculateBracket(string expression);
-    
-    /// <summary>
-    /// Returns the cosine value of a specific angle in radians
-    /// </summary>
-    /// <param name="angle">Angle in radians</param>
-    /// <returns>The cosine value of <paramref name="angle"/></returns>
-    string SolveExpression(string expression);
-}
+    /// <param name="expression">String with mathematical expression</param>
+    /// <returns>Results in brackets<paramref name="expression"/></returns>
+    string CalculateDeepestBrackets(string expression);
 
+    /// <summary>
+    /// Returns the result of a single expression without brackets.
+    /// Use for the solving expression s in brackets with no child brackets.
+    /// </summary>
+    /// <param name="expression">Expression for the solution</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    string SolveExpression(string expression);
+
+    /// <summary>
+    /// Returns the result of the entire expression.
+    /// </summary>
+    /// <param name="expression">Expression for the solution</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    decimal SolveWholeExpression(string expression);
+
+    /// <summary>
+    /// Returns true if brackets in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool BracketIn(string expression);
+
+    /// <summary>
+    /// Returns true if add function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool AddIn(string expression);
+
+    /// <summary>
+    /// Returns true if subtract function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool SubtractIn(string expression);
+
+    /// <summary>
+    /// Returns true if multiply function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool MultiplyIn(string expression);
+
+    /// <summary>
+    /// Returns true if divide function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool DivideIn(string expression);
+
+    /// <summary>
+    /// Returns true if factorial function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool FactorialIn(string expression);
+
+    /// <summary>
+    /// Returns true if power function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool PowerIn(string expression);
+
+    /// <summary>
+    /// Returns true if root function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool RootIn(string expression);
+
+    /// <summary>
+    /// Returns true if log function in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool LogIn(string expression);
+
+    /// <summary>
+    /// Returns true if trigonometric functions in expression
+    ///         false if not
+    /// </summary>
+    /// <param name="expression">Expression to check</param>
+    /// <returns>Solution of the expression <paramref name="expression"/></returns>
+    bool GonFuncsIn(string expression);
+}
